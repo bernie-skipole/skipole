4bff37002762050a02fc69591917c7cdb2f41451
skipole <bernie@skipole.co.uk>
2017-12-09 18:12:40 +0000

    traditional1 meter done

--- projectfiles/lib/static/widgets/svgmeters-0.1.0.js
+++ projectfiles/lib/static/widgets/svgmeters-0.1.0.js
@@ -119,7 +119,64 @@ SKIPOLE.svgmeters.Vertical1.prototype.setvalues = function (fieldlist, result) {
     };
 
 
+SKIPOLE.svgmeters.Traditional1 = function (widg_id, error_message, fieldmap) {
+    SKIPOLE.BaseWidget.call(this, widg_id, error_message, fieldmap);
+    this.display_errors = false;
+    };
+SKIPOLE.svgmeters.Traditional1.prototype = Object.create(SKIPOLE.BaseWidget.prototype);
+SKIPOLE.svgmeters.Traditional1.prototype.constructor = SKIPOLE.svgmeters.Traditional1;
+SKIPOLE.svgmeters.Traditional1.prototype.setvalues = function (fieldlist, result) {
+    if (!this.widg_id) {
+        return;
+        }
+    this.set_attribute('transform', 'transform', result, fieldlist);
+    var the_widg = this.widg;
+    // set the fill attribute of the polygon
+    var arrow = the_widg.find('polygon');
+    if (arrow == undefined) {
+        return;
+        }
+    var circle = the_widg.find('circle');
+    if (circle == undefined) {
+        return;
+        }
+    var arrow_stroke = this.fieldarg_in_result('arrow_stroke', result, fieldlist);
+    if (arrow_stroke) {
+        arrow.attr('stroke', arrow_stroke);
+        circle.attr('stroke', arrow_stroke);
+        }
 
+    // set the measurement
+    var measurement = this.fieldarg_in_result('measurement', result, fieldlist);
+    if (measurement == undefined) {
+        return;
+        }
+
+    var minvalue = this.fieldvalues["minvalue"];
+    var maxvalue = this.fieldvalues["maxvalue"];
+    if (minvalue == undefined) {
+        return;
+        }
+    if (maxvalue == undefined) {
+        return;
+        }
+
+    var m = parseFloat(measurement);
+    var mn = parseFloat(minvalue);
+    var mx = parseFloat(maxvalue);
+
+    if (m >= mx) {
+        arrow.attr('transform', "rotate(60 350 350)");
+        return;
+        }
+    if (m <= mn) {
+        arrow.attr('transform', "rotate(-60 350 350)");
+        return;
+        }
+
+    var scale = Math.round((m-mn)*120/(mx-mn) - 60);
+    arrow.attr('transform', "rotate(" + scale + " 350 350)");
+    };
 
 
 
