e8a7a7a4b8e612097cbc326c38ce2e6debe82404
skipole <42965323+bernie-skipole@users.noreply.github.com>
2018-01-18 18:57:31 +0000

    working on chart1 widget

--- projectfiles/lib/static/widgets/svggraphs-0.1.0.js
+++ projectfiles/lib/static/widgets/svggraphs-0.1.0.js
@@ -29,4 +29,59 @@
 
 SKIPOLE.svggraphs = {};
 
+SKIPOLE.svggraphs.Chart1 = function (widg_id, error_message, fieldmap) {
+    SKIPOLE.BaseWidget.call(this, widg_id, error_message, fieldmap);
+    this.display_errors = false;
+    };
+SKIPOLE.svggraphs.Chart1.prototype = Object.create(SKIPOLE.BaseWidget.prototype);
+SKIPOLE.svggraphs.Chart1.prototype.constructor = SKIPOLE.svggraphs.Chart1;
+SKIPOLE.svggraphs.Chart1.prototype.setvalues = function (fieldlist, result) {
+    if (!this.widg_id) {
+        return;
+        }
+    this.set_attribute('transform', 'transform', result, fieldlist);
+
+    var the_widg = this.widg;
+    // set the chart values
+
+    var values = this.fieldarg_in_result('values', result, fieldlist);
+    if (values == undefined) {
+        return;
+        }
+
+    var number_of_points = values.length;
+
+    if (!number_of_points) {
+        return;
+        }
+
+    // calculate points from these values
+
+    var xpoint = 515;
+    var points = "";
+
+    values.reverse();
+
+    for (pt = 0; pt < number_of_points; pt++) {
+        var ypoint = values[pt];
+        xpoint = xpoint-10;
+        if (xpoint < 5) {
+            break;
+            }
+        points = points + " " + xpoint + ",";
+        if (ypoint > 100){
+            points = points + "5";
+        else if (ypoint < -100) {
+            points = points + "205";
+            }
+        else {
+            points = points + (105-ypoint);
+            }
+
+    var line = the_widg.find('polyline');
+    if (line == undefined) {
+        return;
+        }
+    line.attr('points', points);
+    };
 
