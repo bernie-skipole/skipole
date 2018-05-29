8e238ccb2a88ce28669e61d33e07c6669bb8a57a
skipole <bernie@skipole.co.uk>
2018-05-29 12:11:58 +0100

    added first test setups

---
+++ tests/newproj/conftest.py
@@ -0,0 +1,34 @@
+##########################################################
+#
+# These tests are run using the python3 version of py.test
+#
+# which, when using debian, is the script py.test-3
+# and with python3-pytest installed
+#
+# py.test-3 should be run from the 'skipole' directory
+#
+# This conftest.py file contains the fixture 'project'
+# which loads the project to be tested into the framework
+#
+##########################################################
+
+import os
+
+import pytest
+
+# the framework needs to know the directory where projectfiles are held
+# which is normally 'projectfiles' beneath three directories up from this file
+
+projectfiles = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'projectfiles')
+
+@pytest.fixture(scope="session")
+def project():
+    """Sets the test project being run by skipole.
+       Returns the project name"""
+    import skipoles
+    # this line sets the test project to 'newproj', and also
+    # calls the user 'start_project' function
+    testproject = skipoles.WSGIApplication("newproj", {}, projectfiles)
+    return testproject.proj_ident
+
+
