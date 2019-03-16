9b2dfad1ceb94aeda6102c728813c564c188ea07
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-03-16 19:30:08 +0000

    Working on ability to edit skiadmin and skis

--- projectfiles/skis/code/skis.py
+++ projectfiles/skis/code/skis.py
@@ -1,9 +1,13 @@
+
 # This skis project is added to other projects and consists of
-# javascript and css files.
+# javascript and css files. As it is essentially a 'library'
+# it is usually given a URL of /lib when added - but this is
+# not a necessity, just convention.
 
 
-from skipole import WSGIApplication
+from skipole import WSGIApplication, set_debug
 
+PROJECT = 'skis'
 
 def start_call(called_ident, skicall):
     "This project has no start_call functionality"
@@ -20,7 +24,46 @@ def end_call(page_ident, page_type, skicall):
 
 def makeapp(projectfiles, proj_data={}):
     """This function allows projectfiles and proj_data to be set into the skis application"""
-    application = WSGIApplication('skis', projectfiles, proj_data, start_call, submit_data, end_call, url="/skis")
+
+    # The WSGIApplication created here has a URL of "/", however when this
+    # application is added to another, it is generally given a URL of "/lib"
+    # in the application.add_project method which overwrites the URL given here
+
+    application = WSGIApplication(PROJECT, projectfiles, proj_data, start_call, submit_data, end_call, url="/")
     return application
 
+
+# In normal use, the skis project is only imported, so the following is never run.
+# However if you want to use skiadmin to modify it, then it could be run like any other
+# project.
+
+
+if __name__ == "__main__":
+
+    # If called as a script, this portion runs the python wsgiref.simple_server
+    # and serves the project. Typically you would do this with the 'skiadmin'
+    # sub project added which can be used to develop pages for your project
+
+    import sys, os
+    from wsgiref.simple_server import make_server
+
+    PROJECTFILES = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+    application = makeapp(PROJECTFILES)
+
+    set_debug(True)
+    skiadmin_code = os.path.join(PROJECTFILES, 'skiadmin', 'code')
+    if skiadmin_code not in sys.path:
+        sys.path.append(skiadmin_code)
+    import skiadmin
+    skiadmin_application = skiadmin.makeapp(PROJECTFILES)
+    application.add_project(skiadmin_application, url='/skiadmin')
+
+    # serve the application
+    host = "127.0.0.1"
+    port = 8000
+
+    httpd = make_server(host, port, application)
+    print("Serving %s on port %s. Call http://localhost:8000/skiadmin to edit." % (PROJECT, port))
+    httpd.serve_forever()
 
