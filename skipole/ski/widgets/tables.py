f63a844f00e0e6491cf93d3e8316ca52cca3e461
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-03-25 17:17:49 +0000

    Changing lincence to MIT

--- skipole/ski/widgets/tables.py
+++ skipole/ski/widgets/tables.py
@@ -1,28 +1,4 @@
-####### SKIPOLE WEB FRAMEWORK #######
-#
-# tables.py  - Contains tables of text and links
-#
-# This file is part of the Skipole web framework
-#
-# Date : 20140430
-#
-# Author : Bernard Czenkusz
-# Email  : bernie@skipole.co.uk
-#
-#
-#   Copyright 2014 Bernard Czenkusz
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+
 
 from .. import skiboot, tag
 from . import Widget, FieldArg, FieldArgList, FieldArgTable, FieldArgDict
