e6aac645d30fca08c02d53a30dee4f7f62646ede
skipole <bernie@skipole.co.uk>
2019-03-25 17:17:49 +0000

    Changing lincence to MIT

--- skipole/ski/responders/checkers.py
+++ skipole/ski/responders/checkers.py
@@ -1,28 +1,3 @@
-####### SKIPOLE WEB FRAMEWORK #######
-#
-# checkers.py of responders package  - Defines responders used for checking and validation
-#
-# This file is part of the Skipole web framework
-#
-# Date : 20130205
-#
-# Author : Bernard Czenkusz
-# Email  : bernie@skipole.co.uk
-#
-#
-#   Copyright 2013 Bernard Czenkusz
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
 
 
 """A Respond object instance is placed in a Respond page,
--- skipole/skilift/editfolder.py
+++ skipole/skilift/editfolder.py
@@ -1,28 +1,3 @@
-####### SKIPOLE WEB FRAMEWORK #######
-#
-# editfolder.py of skilift package  - functions for editing a folder
-#
-# This file is part of the Skipole web framework
-#
-# Date : 20160509
-#
-# Author : Bernard Czenkusz
-# Email  : bernie@skipole.co.uk
-#
-#
-#   Copyright 2013 Bernard Czenkusz
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
 
 
 """Functions for editing a folder"""
