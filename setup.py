b676b5816014969fc4a16f300b3888d1a5d56825
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-06-13 23:17:31 +0100

    started version 3.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skipole",
-    version="3.0.9",
+    version="3.1.0",
     author="Bernard Czenkusz",
     author_email="bernie@skipole.co.uk",
     description="A WSGI Application generator",
