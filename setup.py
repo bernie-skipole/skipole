283a8ffbdb5af64bc4d0d9798294c9ddd59704b6
skipole <42965323+bernie-skipole@users.noreply.github.com>
2019-08-24 00:51:58 +0100

    working on downloading defaults.json

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skipole",
-    version="3.5.0",
+    version="3.5.1",
     author="Bernard Czenkusz",
     author_email="bernie@skipole.co.uk",
     description="A WSGI Application generator",
