ddbd7284366e2dadc05a9c75c6fa830060085b60
skipole <bernie@skipole.co.uk>
2019-08-25 16:39:24 +0100

    working on downloading defaults.json

--- skipole/template_projects/skiadmin/code/skiadminpackages/utils.py
+++ skipole/template_projects/skiadmin/code/skiadminpackages/utils.py
@@ -7,7 +7,7 @@ from skipole import skilift
 
 
 def clear_call_data(call_data, keep=None):
-    "Clears call data apart from set of required values"
+    "Clears call data apart from set of required values, and anything in the keep list"
     required = ['editedprojname',
                 'editedprojurl',
                 'editedprojversion',
@@ -16,7 +16,13 @@ def clear_call_data(call_data, keep=None):
                 'extend_nav_buttons',
                 'caller_ident']
     if keep:
-        required.extend(keep)
+        if isinstance(keep, str) and (keep not in required):
+            required.append(keep)
+        else:
+            # presume keep is a list of items
+            for item in keep:
+                if item not in required:
+                    required.append(item)
     temp_storage = {key:value for key,value in call_data.items() if key in required}
     call_data.clear()
     call_data.update(temp_storage)
