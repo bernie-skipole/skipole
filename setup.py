f8f84131a97de776710a7d54dd87f30c851b7ed0
bernie-skipole <bernie@skipole.co.uk>
2019-12-10 12:00:15 +0000

    version 3.5.8 set in setup.py for sending to pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skipole",
-    version="3.5.7",
+    version="3.5.8",
     author="Bernard Czenkusz",
     author_email="bernie@skipole.co.uk",
     description="A WSGI Application generator",
