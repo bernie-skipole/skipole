75d2cf1190567445a0340172b1a71e67f7392c1e
bernie-skipole <bernie@skipole.co.uk>
2020-06-12 09:37:41 +0100

    Up to version 3.8.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skipole",
-    version="3.8.0",
+    version="3.8.1",
     author="Bernard Czenkusz",
     author_email="bernie@skipole.co.uk",
     description="A WSGI Application generator",
