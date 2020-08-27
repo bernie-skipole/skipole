c73bd7078ec45be86557e594fe10e5abd33f690a
bernie-skipole <bernie@skipole.co.uk>
2020-08-27 16:10:58 +0100

    started version 4.0.9

--- skipole/ski/skiboot.py
+++ skipole/ski/skiboot.py
@@ -10,7 +10,7 @@ import os, copy, collections
 # Configuration defaults
 
 _CFG = {
-"version"         : "4.0.8",             # The skipole version
+"version"         : "4.0.9",             # The skipole version
 "default_language": 'en',                # The default language of the project
 "debug"           : False                # The debug mode, True shows exceptions on server error
 }
