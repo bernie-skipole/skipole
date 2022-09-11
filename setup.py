594f3dcb368b4cd2dfc64a76acef979babd10009
Bernard Czenkusz <bernie@skipole.co.uk>
2022-09-11 20:23:42 +0100

    Started version 5.4.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skipole",
-    version="5.4.7",
+    version="5.4.8",
     author="Bernard Czenkusz",
     author_email="bernie@skipole.co.uk",
     description="A WSGI Application generator",
